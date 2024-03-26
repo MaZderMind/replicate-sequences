beadffd157d2d1a0e7f5c5d7a936e3eb3e7a7146
Peter Körner <pkoerner@seibert-media.net>
2024-03-26 14:01:25 +0100

    release docker v2.1

--- docker/push-container.sh
+++ docker/push-container.sh
@@ -2,4 +2,4 @@
 
 # run `docker buildx create --use` before first use
 docker buildx build --platform linux/amd64,linux/arm64 -t mazdermind/replicate-sequences:latest --push -f ./Dockerfile ..
-docker buildx build --platform linux/amd64,linux/arm64 -t mazdermind/replicate-sequences:v2 --push -f ./Dockerfile ..
+docker buildx build --platform linux/amd64,linux/arm64 -t mazdermind/replicate-sequences:v2.1 --push -f ./Dockerfile ..
